e6b8c955d5205f9295fe85c9e2891a7aebb4bd94
reemalharbi381 <reemalharbi381@gmail.com>
2026-01-08 09:50:05 +0300

    fix:add port binding

--- main.py
+++ main.py
@@ -53,3 +53,9 @@ def get_invoices(c_id: int):
 #final update check
 #finalS 
 #final
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
